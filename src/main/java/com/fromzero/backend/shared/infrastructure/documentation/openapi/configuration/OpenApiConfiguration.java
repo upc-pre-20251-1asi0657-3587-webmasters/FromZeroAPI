83066282c4b2fa8b1455d0891c21a20bfffe7499
Sr-Anonymus-maker <carlosleonrioja@gmail.com>
2025-05-11 16:34:21 -0500

    fix: declaration of unused application properties

--- src/main/java/com/fromzero/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/fromzero/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -7,11 +7,19 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
     @Bean
     public OpenAPI learningPlatformOpenApi() {
         // General configuration
